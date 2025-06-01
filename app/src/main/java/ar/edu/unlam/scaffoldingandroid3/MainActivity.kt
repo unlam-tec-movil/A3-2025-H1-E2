01c9f2bfb4068c78dbcedd464b16d2b1b3d3b785
FranFox100 <fyanezpioch292@alumno.unlam.edu.ar>
2025-06-01 13:29:55 -0300

    ktlint fix

--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -32,64 +28,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
             val controller = rememberNavController()
 
             ScaffoldingAndroid3Theme {
-
                 val navBackStackEntry = controller.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry.value?.destination?.route
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
                     NavHost(navController = controller, startDestination = NavigationRoutes.MapScreen.route) {
+                        composable(NavigationRoutes.MapScreen.route) {
+                            MapScreen(controller)
+                        }
 
-                     composable (NavigationRoutes.MapScreen.route){
-
-                         MapScreen(controller)
-
-                     }
-
-                        composable (NavigationRoutes.ProfileScreen.route) {
-
+                        composable(NavigationRoutes.ProfileScreen.route) {
                             ProfileScreen()
-
-
                         }
 
-                        composable (NavigationRoutes.CreateMonument.route) {
-
+                        composable(NavigationRoutes.CreateMonument.route) {
                             CreateMonumentScreen()
-
                         }
 
-                        composable (NavigationRoutes.AjustesScreen.route) {
-
+                        composable(NavigationRoutes.AjustesScreen.route) {
                             AjustesScreen()
-
                         }
 
-                        composable (NavigationRoutes.NotificacionesScreen.route) {
-
+                        composable(NavigationRoutes.NotificacionesScreen.route) {
                             NotificacionesScreen()
-
                         }
 
-                        composable (NavigationRoutes.ComunidadScreen.route) {
-
+                        composable(NavigationRoutes.ComunidadScreen.route) {
                             ComunidadScreen()
-
                         }
 
-                        composable (NavigationRoutes.MonumentosScreen.route) {
-
+                        composable(NavigationRoutes.MonumentosScreen.route) {
                             MonumentosScreen()
-
                         }
 
-                        composable (NavigationRoutes.AlbumScreen.route) {  }
-
-
+                        composable(NavigationRoutes.AlbumScreen.route) { }
                     }
                 }
             }
--- app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/screens/MapScreen.kt
+++ app/src/main/java/ar/edu/unlam/scaffoldingandroid3/ui/screens/MapScreen.kt
@@ -7,16 +7,13 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -30,8 +27,6 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -43,7 +38,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -82,23 +76,24 @@ fun MapScreen(
     }) {
         Column(
             modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-
             AnimatedVisibility(visible = !state, enter = fadeIn(), exit = fadeOut()) {
                 Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .animateEnterExit(enter = slideInVertically(), exit = slideOutVertically())
+                    modifier =
+                        Modifier
+                            .fillMaxSize()
+                            .animateEnterExit(enter = slideInVertically(), exit = slideOutVertically()),
                 ) {
                     MonumentMap(modifier = Modifier.fillMaxSize())
                 }
             }
             AnimatedVisibility(visible = state, enter = fadeIn(), exit = fadeOut()) {
                 Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .animateEnterExit(enter = slideInVertically(), exit = slideOutVertically())
+                    modifier =
+                        Modifier
+                            .fillMaxSize()
+                            .animateEnterExit(enter = slideInVertically(), exit = slideOutVertically()),
                 ) {
                     NavMenu(controller)
                 }
@@ -134,7 +129,6 @@ fun NavMenu(navController: NavHostController) {
     // Se trata de ua columna compuesta por filas de BotonMenuNav(controller, ruta, icon, descripcion)
     // BotonMenuNav(controller = navController, ruta = NavigationRoutes.ProfileScreen, icon =
     // Icons.Default.AccountCircle, descripcion = "Perfil")
-
 
     Column(
         modifier =
